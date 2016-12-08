8520442e0b342310e73ac9af6706dc8287ed506b
HazemAhmed95 <hazem_yones95cs@hotmail.com>
2016-12-09 01:43:29 +0200

    Reorginzed the code in app.routes added passport to packge.json

--- public/app/app.routes.js
+++ public/app/app.routes.js
@@ -1,11 +1,22 @@
-angular.module('app.routes', ['ngRoute']).config(function ($routeProvider, $locationProvider) {
-    $routeProvider.when('/index', {
-        templateUrl: 'app/views/pages/products.html',
-        controller: 'productController'
-        , controllerAs: 'main'
-    }).when('/:id',{
-        templateUrl:'app/views/pages/viewDetail.html',
-        controller:'viewController'
-    });
+angular.module('app.routes', ['ngRoute'])
+	.config(function ($routeProvider, $locationProvider) {
+		$routeProvider
+		
+			.when('/', {
+				templateUrl: 'app/views/pages/products.html',
+				controller: 'productController'
+			, controllerAs: 'main'
+			})
+		
+			.when('/index', {
+				templateUrl: 'app/views/pages/products.html',
+				controller: 'productController'
+				, controllerAs: 'main'
+			})
+		
+			.when('/:id',{
+				templateUrl:'app/views/pages/viewDetail.html',
+				controller:'viewController'
+		});
     $locationProvider.html5Mode(true);
 });
