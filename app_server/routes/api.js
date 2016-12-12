a4132750ec8d53913c314049d2b35f367e429729
HazemAhmed95 <hazem_yones95cs@hotmail.com>
2016-12-12 20:22:33 +0200

    Added tokken based Auth to the backend the securty layer the user regstiration user login refactroing the routes

--- app_server/routes/api.js
+++ app_server/routes/api.js
@@ -1,11 +1,15 @@
 var express    = require('express'),
     router     = express.Router(),  
     mysql      = require('mysql'),
-    database   = require('../models/database');
-
+    database   = require('../models/database'),
+    bcrypt 		= require('bcryptjs'),
+    Secret     = "LeahYaZmanMsbtnashAbrya2",
+    jwt 			= require('jsonwebtoken');
 var connection = database.connectDatabase();
 
-router.get('/products', function(req, res) {
+
+router.route('/products')
+.get(verifyUser,function(req, res) {
    var query = "SELECT * FROM Products";
    connection.query(query, function(err, rows, fields) {
       if(err) {
@@ -15,5 +19,82 @@ router.get('/products', function(req, res) {
       }
    });
 });
+
+//////////////////
+// USER SIGN UP///
+//////////////////
+router.post("/signup", function (req, res) {
+			connection.query("SELECT * FROM Users WHERE username = ?", [req.body.username], function (err, rows) {
+					if (err) return done(err);
+					if (rows.length)
+						 res.json({message: 'User Name Already Exists'});
+					else {
+						var newUserMysql = {
+							username: req.body.username,
+							password: bcrypt.hashSync(req.body.password, 8)
+						};
+						var insertQuery = "INSERT INTO Users ( username, password ) values (?,?)";
+						connection.query(insertQuery, [newUserMysql.username, newUserMysql.password], function (err,rows) {
+							if (err) throw err;
+								 res.json({message: 'Registration Successful'});
+						})
+					}
+				});
+			})
+
+//////////////////
+// USER lOG IN////
+//////////////////
+router.post('/login', function (req, res) {
+	connection.query("SELECT * FROM Users WHERE username = ?", [req.body.username], function (err, rows) {
+		if (err) return done(err);
+		if (!rows.length)
+			 res.json({	message: 'Incorrect User Name'});
+		else if (rows.length) {
+			// check if password matches
+			if (!bcrypt.compareSync(req.body.password, rows[0].password)) {
+				res.json({
+					message: 'Incorrect Password'
+				});
+			}
+			else {
+				// create a token
+				var token = jwt.sign(req.body.username, Secret, {})
+				res.json({
+					  success: true,
+					  message: 'Welcome',
+					  token: token
+				});
+			}
+		}
+	});
+});
+
+////////////////////////////////////////
+///route middleware to verify a token///
+////////////////////////////////////////
+function verifyUser(req, res, next) {
+	 // check header or url parameters or post parameters for token
+  var token = req.body.token || req.query.token || req.headers['x-access-token'];
+  // decode token
+  if (token) {
+		 jwt.verify(token,Secret,function(err,decoded){
+			  if (err)
+					 return res.json({ success: false, message: 'Failed to authenticate token.' });
+			  else {
+					 req.decoded = decoded; 
+					 console.log(req.decoded);
+					 next();
+			  }
+		 })
+  }
+	 else {
+		  return res.status(403).send({ 
+        success: false, 
+        message: 'No token provided.' 
+		  });
+	 }
+
+};
 
 module.exports = router;
--- server.js
+++ server.js
@@ -9,9 +9,6 @@ var morgan     = require('morgan'); 		// used to see requests
 var config 	   = require('./config');
 var path 	     = require('path');
 
-var session  = require('express-session');
-var flash    = require('connect-flash');
-var passport        = require('./app_server/models/passport')(passport);
 // APP CONFIGURATION ==================
 // ====================================
 // use body parser so we can grab information from POST requests
@@ -37,19 +34,11 @@ app.use(express.static(__dirname + '/public'));
 
 // ROUTES FOR OUR API =================
 // ====================================
-// required for passport
-app.use(session({
-	secret: 'vidyapathaisalwaysrunning',
-	resave: true,
-	saveUninitialized: true
- } )); // session secret
-app.use(passport.initialize());
-app.use(passport.session()); // persistent login sessions
-app.use(flash()); // use connect-flash for flash messages stored in session
+
 
 // API ROUTES ------------------------
 var apiRoutes = require('./app_server/routes/api');
-app.use('/api', apiRoutes)(passport);
+app.use('/api', apiRoutes);
 
 // MAIN CATCHALL ROUTE --------------- 
 // SEND USERS TO FRONTEND ------------
