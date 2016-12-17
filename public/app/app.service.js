fa7d898db3386b18528b1d502384f905ceab821c
HazemAhmed95 <hazem_yones95cs@hotmail.com>
2016-12-18 01:03:33 +0200

    tabs <3

--- public/app/app.service.js
+++ public/app/app.service.js
@@ -2,7 +2,7 @@ angular.module('app.services', [])
 	
 	
 	
-	.factory('Product', function ($http) {
+.factory('Product', function ($http) {
     return {
         get: function () {
             return $http.get('http://localhost:8080/api/products');
@@ -12,20 +12,32 @@ angular.module('app.services', [])
 
 
 .factory('users',function($http){
+	
 	var usersFactory = {}
+	
 	usersFactory.signUp = function (username,password ,email) {
 		
-	return $http.post('http://localhost:8080/api/signup',{
-			 username : username,
-			 password : password,
-			 email : email
-		 })
+	
+		return $http.post('http://localhost:8080/api/signup',{
+		
+			username : username,
+			
+			password : password,
+			
+			email : email
+		 
+		})
+		
 			.success(function(response) {
-		  return response;	
+		  
+			return response;	
+		
 		});
 	
 	
+	
 	}
+	
 	return usersFactory;
 
 })
@@ -38,33 +50,59 @@ angular.module('app.services', [])
         }
     };
 })
+
+
 .factory('Auth', function($http, $q, AuthToken) {
 	
+	
 	var authFactory = {};
+	
 	authFactory.signUpText = "Sign up";    
+	
 	authFactory.logInText = "Log in"; 
+	
 	authFactory.logedIn= false;
+	
 	authFactory.login = function(username, password) {
+	
 		return $http.post('http://localhost:8080/api/login', {
-			 username : username,
-			 password : password
+		
+			username : username,
+			
+			password : password
+		
 		})
-		.success(function(response){
+		
+			.success(function(response){
+			
 			AuthToken.setToken(response.token);
+			
 			if(response.success == true){
 			
-					authFactory.signUpText = username;
-					authFactory.logInText = "Log out";
-					authFactory.logedIn= true;
-						}    
+			
+				authFactory.signUpText = username;
+				
+				authFactory.logInText = "Log out";
+				
+				authFactory.logedIn= true;
+				
+			}    
+			
 			return response;
+		
 		})
+	
 	}
 	
 	
+	
+	
 	authFactory.logout = function() { 
-  			AuthToken.setToken();
-  }
+  	
+		AuthToken.setToken();
+  
+	}
+	
 	return authFactory;
 })
 			
@@ -72,55 +110,88 @@ angular.module('app.services', [])
 // inject $window to store token client-side
 .factory('AuthToken', function($window) {
 
-	 var authTokenFactory = {};
+	
+	var authTokenFactory = {};
 
 	 // get the token
-  authTokenFactory.getToken = function() {
-	  return $window.localStorage.getItem('token');
-  }
+
+	authTokenFactory.getToken = function() {
+	
+		return $window.localStorage.getItem('token');
+  
+	}
 	 // set the token or clear the token
-  authTokenFactory.setToken = function(token){
-	  if(token){
-		  $window.localStorage.setItem('token', token);
-		  console.log("saved");
-	  }
-		  else 
-		  $window.localStorage.removeItem('token');
-  }
+  
+	authTokenFactory.setToken = function(token){
+	
+		if(token){
+		
+			$window.localStorage.setItem('token', token);
+		  
+			console.log("saved");
+	  
+		}
+		
+		else 
+		
+			$window.localStorage.removeItem('token');
+  
+	}
   
  
+	
 	return authTokenFactory;
 
- })
+ 
+})
 			
 // application configuration to integrate token into requests
 .factory('AuthInterceptor', function($q, AuthToken){
 
- var interceptorFactory = {};
 
-// this will happen on all HTTP requests
- interceptorFactory.request = function(config) {
-	 //get the tokken from the localstorage
-	var token = AuthToken.getToken();
-	 // if the token exists, add it to the header as x-access-token
- 	if (token)
-	 config.headers['x-access-token'] = token;
-	 
-	 return config;
-	 
- }
+	var interceptorFactory = {};
+
+
+	// this will happen on all HTTP requests
  
- interceptorFactory.responseError = function(response) {
-
-	 // if our server returns a 403 forbidden response
-	 if (response.status == 403)
-	 	$location.path('/login');
-
-	 // return the errors from the server as a promise
-	 return $q.reject(response);
- 
- };
+	interceptorFactory.request = function(config) {
 	
- return interceptorFactory;
+		//get the tokken from the localstorage
+	
+		var token = AuthToken.getToken();
+	 
+		// if the token exists, add it to the header as x-access-token
+ 	
+		if (token)
+	 
+			config.headers['x-access-token'] = token;
+	 
+	 
+		return config;
+	 
+ 
+	}
+ 
+ 
+	interceptorFactory.responseError = function(response) {
 
- });		
+	
+		// if our server returns a 403 forbidden response
+	 
+		if (response.status == 403)
+	 	
+			$location.path('/login');
+
+	 
+		// return the errors from the server as a promise
+	 
+		return $q.reject(response);
+ 
+ 
+	};
+	
+ 
+	return interceptorFactory;
+
+
+});		
--- public/app/controllers/mainCtrl.js
+++ public/app/controllers/mainCtrl.js
@@ -7,24 +7,43 @@
 	 
 .controller('productController', ['$scope', 'Product', '$http', function($scope, Product, $http) {
 
-     Product.get().then(function (res) {
-         $scope.products = res.data;
-         $scope.getImagePath = function (imageName) {
-             return "images/" + imageName;
-         };
-         if (container !== null) {
-             container = $scope.products;
-         }
+     
+	Product.get().then(function (res) {
+         
+		  $scope.products = res.data;
+   
+		  $scope.getImagePath = function (imageName) {
+      
+			return "images/" + imageName;
+         
+		};
+      
+		if (container !== null) {
+      
+			container = $scope.products;
         
-         $scope.addToCart = function (id) {
-             if (container[id].quantity > 0) {
-                 $scope.products[id].quantity--;
-                 $http.post('http://localhost:8080/api/cart', container[id]).success(function (res) {
-                     console.log(res.message);
-                 });
-             }
-             else {
-                 alert("La2");
+		}
+        
+      
+		$scope.addToCart = function (id) {
+      
+		
+		
+			if (container[id].quantity > 0) {
+                
+					$scope.products[id].quantity--;
+               
+					$http.post('http://localhost:8080/api/cart', container[id]).success(function (res) {
+               
+					console.log(res.message);
+                
+					});
+             
+				}
+            
+				else {
+            
+					alert("La2");
              }
          };
      });
@@ -43,29 +62,40 @@
 .controller('loginController', ['$scope', '$filter', '$location', 'users','Auth','$timeout',function ($scope, $filter, $location, users,Auth,$timeout) {
 	
 	 
-	
+	// calls the auth factory to make a login
 	$scope.loginButton = function () {
 			
-		 Auth.login($scope.username, $scope.password)
+		
+		Auth.login($scope.username, $scope.password)
 				
-				 .success(function (response) {
-					 $scope.res = response;
-					 if($scope.res.success == true){
-							$timeout(function(){
+			 .success(function (response) {
+					 
+			 $scope.res = response;
+			//if the login was success redirect the user to the index page		 
+			 if($scope.res.success == true){
+							
+						 $timeout(function(){
+						   
 							 $location.path("/index");
-
-						  },1000);
-						}
-					});
+							
+						 },1000);
+							
+			 }
+				
+		});
 		  			
 		         
 		 
-		  };
+		  
+	};
 
 
+	
 	$scope.signupButton = function () {
-           $location.path("/register");
-       };
+   
+		$location.path("/register");
+      
+	};
 
 
 
@@ -73,39 +103,64 @@
  
 .controller('userNavBar',['$scope','Auth','$rootScope','$location',function($scope,Auth,$rootScope,$location){
 
-		$rootScope.$on('$routeChangeStart', function(){	
-			$scope.login = Auth.logInText;
-			$scope.signup = Auth.signUpText;
-		})
+	//on every route change update the text in the user navbar	
+	$rootScope.$on('$routeChangeStart', function(){	
+			
+		$scope.login = Auth.logInText;
+			
+		$scope.signup = Auth.signUpText;
+		
+	})
 
-    $scope.toSignup = function(){
-     if(!Auth.logedIn)
-		  $location.path("/register");
-	 }
-	  $scope.toLogin = function(){
-     if(Auth.logedIn)
-		  $location.path("/index");
-		  else 
-			 $location.path("/login");
-	 }
+   //the sign up text only redirect if the user is not loged in  
+	$scope.toSignup = function(){
+   
+		if(!Auth.logedIn)
+		
+			$location.path("/register");
+	
+	}
+	//if the user is loged in call the logout function other wise redirect to login page  
+	$scope.toLogin = function(){
+     
+		if(Auth.logedIn)
+		//call logout
+			
+		  
+		else 
+		
+			$location.path("/login");
+	 
+	}
 
 }]) 
  
 .controller('registerController', ['$scope', '$filter', '$location', 'users', function ($scope, $filter, $location, users) {
 
-			 $scope.username = "";
+			 
+	$scope.username = "";
 
-			 $scope.loginButton = function () {
-						$location.path("/login");
-				  };
+			 
+	$scope.loginButton = function () {
+						
+		$location.path("/login");
+				  
+	};
 
-			 $scope.signupButton = function () {
-						users.signUp($scope.username, $scope.password, $scope.email)
-							.success(function (response) {
-									$scope.res = response;
-						});
-				  };
- }])
+			 
+	$scope.signupButton = function () {
+						
+		users.signUp($scope.username, $scope.password, $scope.email)
+							
+			.success(function (response) {
+									
+			$scope.res = response;
+						
+		});
+				  
+	};
+
+}])
  
  
  
